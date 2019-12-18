95e08d25479dacdb42bb3863c3a6d929a6604daf
elahi <elahi@ip-129-70-146-56.wlan.dyn.cit-ec.net>
2019-12-18 18:46:24 +0100

    first commit

---
+++ src/java/browser/termallod/utils/FileRelatedUtils.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package browser.termallod.utils;
+
+import java.io.File;
+import java.io.FileFilter;
+import org.apache.commons.io.filefilter.WildcardFileFilter;
+
+/**
+ *
+ * @author elahi
+ */
+public class FileRelatedUtils {
+    
+     public static File[] getFiles(String fileDir, String ntriple) {
+         System.out.println("file name"+fileDir);
+        File dir = new File(fileDir);
+        FileFilter fileFilter = new WildcardFileFilter("*" + ntriple);
+        File[] files = dir.listFiles(fileFilter);
+        return files;
+
+    }
+     
+    
+}
